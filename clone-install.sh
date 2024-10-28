4ac1d03494324e11f1cc4a654a0df5913e25a6f4
MMDRZA <info@mmdrza.com>
2024-10-28 21:01:09 +0300

    Update clone-install.sh

--- clone-install.sh
+++ clone-install.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+# Update the system and install required packages
+echo "Updating system and installing necessary packages..."
+sudo apt update -y && sudo apt install nginx curl git -y
 git clone https://github.com/xLLMA/OllamaServer&&cd OllamaServer
+# permission 
 chmod +x install.sh
+# run install file
 bash install.sh
