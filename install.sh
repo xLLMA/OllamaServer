1a3a497aed58dfd518e65eb29b629c551ac4efbf
MMDRZA <info@mmdrza.com>
2024-10-28 22:23:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/xLLMA/OllamaServer.git &&
-cd OllamaServer &&
-python3 pull.py &&
+curl -sSL https://raw.githubusercontent.com/xLLMA/OllamaServer/main/pull.py | python3 &&
 read -p "Enter the desired OLLAMA repository name (e.g., orca-mini): " repo_name &&
 sudo apt update &&
 sudo apt install nginx -y &&
 curl https://ollama.ai/install.sh | sh &&
-OLLAMA_HOST=0.0.0.0:1434 &&
+export OLLAMA_HOST="0.0.0.0:1434" &&
 service ollama start &&
 ollama pull $repo_name &&
 IP=$(curl -s4 ifconfig.me); echo "server { listen $IP:11434; server_name _; location / { proxy_pass http://127.0.0.1:11434; proxy_set_header Host \$host; proxy_set_header X-Real-IP \$remote_addr; }}" | sudo tee /etc/nginx/sites-available/ollma > /dev/null &&
