3ab5160aeb9f9e282c4724351811321ca571f7a4
MMDRZA <info@mmdrza.com>
2024-10-28 22:01:28 +0300

    Update pull.py

--- pull.py
+++ pull.py
@@ -36,53 +36,10 @@ class Libs:
     def display_libraries(self):
         """Display available libraries with their IDs."""
         print(f"{Colors.GREEN}Available Libraries:{Colors.RESET}")
-        for index, (repo_name, repo_url) in enumerate(self.all_libs, start=1):
-            print(f"[{Colors.GREEN}{index}{Colors.RESET}] "
-                  f"{Colors.MAGENTA}{repo_name.upper()}{Colors.RESET} :"
-                  f"{Colors.CYAN} {repo_url}{Colors.RESET}")
-
-    def pull_lib(self):
-        """Pull Ollama Library."""
-        repo_name, repo_url = None, None
-        self.display_libraries()
-        print(f"\n[+] Enter Library {Colors.RED}Number{Colors.RESET} To Pull Library:")
-
-        # get pull number (input)
-        try:
-            pull_number = int(input("[-] Pull > "))
-            # update repo_name and repo_url
-            if pull_number in range(1, len(self.all_libs) + 1):
-                repo_name = self.all_libs[pull_number - 1][0]
-                repo_url = self.all_libs[pull_number - 1][1]
-                print(f"{Colors.CYAN}Library:{Colors.RESET} {repo_name}"
-                      f"\n{Colors.CYAN}Pulling:{Colors.RESET} {repo_url}")
-            else:
-                print(f"{Colors.RED}Invalid selection. Please choose a number from the list.{Colors.RESET}")
-        except ValueError:
-            print(f"{Colors.RED}Please enter a valid number.{Colors.RESET}")
-
-        return repo_name, repo_url
-
-    def save_repo_to_file(self, idLib):
-        """Save the selected repo_name to repo.txt."""
-        if idLib in range(1, len(self.all_libs) + 1):
-            
-            repo_name = self.all_libs[idLib - 1][0]
-            repo_url = self.all_libs[idLib - 1][1]
-            with open("repo.txt", "w") as f:
-                f.write(repo_name)
-            print(f"{Colors.CYAN}Library:{Colors.RESET}{Colors.GREEN} {repo_name}{Colors.RESET}"
-                  f"\n{Colors.CYAN}Pulling:{Colors.RESET} {repo_url}")
-        else:
-            print("Invalid selection. Please choose a valid library number.")
+        for repo_name, repo_url in self.all_libs:
+            print(f"{Colors.GREEN}>{Colors.RESET} {Colors.MAGENTA}{repo_name}{Colors.RESET} : {repo_url}")
 
 
 if __name__ == "__main__":
-    import sys
-
-    libs = Libs()
-    libs.display_libraries()
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        selection = int(sys.argv[1])
-        libs.save_repo_to_file(selection)
-
+    LIBS = Libs()
+    LIBS.display_libraries()
